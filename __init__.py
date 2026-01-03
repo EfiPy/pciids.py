139d70b8aa37eaf2d069326afd4f4a7abf1aa85b
MaxWu <EfiPy.Core@gmail.com>
2026-01-04 00:24:45 +0800

    Add __init__.py in package

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+#
+# Copyright (C) 2026 MaxWu efipy.core@gmail.com All rights reserved.
+#
+# License GPL V2
+#
+from .pciids import PciIds, PciClass
+
+__all__ = ["PciIds", "PciClass"]
